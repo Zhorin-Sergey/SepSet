ccc4cb4fe3759bdc8197dfcc93bd250f43f859b0
Zhorin Sergey <ser4038772@mail.ru>
2016-04-17 13:48:20 +0300

    Самое время добить

--- solutions/Sepset/zSample/f.h
+++ solutions/Sepset/zSample/f.h
@@ -1,5 +1,5 @@
-#ifndef SOLUTIONS_SEPSET_EXP_F_H_
-#define SOLUTIONS_SEPSET_EXP_F_H_
+#ifndef SOLUTIONS_SEPSET_ZSAMPLE_F_H_
+#define SOLUTIONS_SEPSET_ZSAMPLE_F_H_
 
 #include "Sepset.h"
 #include <ctime>
@@ -9,4 +9,4 @@
 void countino(Sepset *set, double *co, int n, int m);
 void preparation(Sepset *set, int n, int m);
 
-#endif  // SOLUTIONS_SEPSET_EXP_F_H_
+#endif  // SOLUTIONS_SEPSET_ZSAMPLE_F_H_
